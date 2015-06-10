9ab50845d844a1d553333228c306a783832ff547
zcoding <wuzijie_1991@163.com>
2015-06-10 21:18:08 +0800

    gulp build

--- build/pixel.js
+++ build/pixel.js
@@ -1,4 +1,33 @@
 
+/**
+ * 卷积运算
+ * @param matrix 矩阵
+ * @param template 模板
+ * @return result 返回矩阵和模板卷积运算的结果
+ */
+function Convolution(matrix, template) {
+    var result = 0;
+    for (var i = 0; i < matrix.length; ++i) {
+      result += matrix[i] * template[i];
+    }
+    return result;
+}
+
+/// 辅助函数
+/**
+ * 升序函数，用于数组排序
+ */
+function ascendingOrder(a, b) {
+  return a - b;
+}
+/**
+ * 降序函数，用于数组排序
+ */
+function descendingOrder(a, b) {
+  return b - a;
+}
+
+
 var Pixel = (function() {
 
   var Pixel = {};
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,21 @@
 var jshint = require('gulp-jshint');
 var gulp   = require('gulp');
+var concat = require('gulp-concat');
+
+gulp.task('scripts', function() {
+  return gulp.src('./lib/*.js')
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('./dist/'));
+});
 
 gulp.task('lint', function() {
   return gulp.src('./src/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
+});
+
+gulp.task('build', function() {
+  return gulp.src(['./src/utils.js', './src/pixel.js'])
+    .pipe(concat('pixel.js'))
+    .pipe(gulp.dest('./build/'));
 });
