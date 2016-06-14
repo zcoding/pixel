e05312bca30dcbbbe141c1ce4d841cff8ee4b2ac
zcoding <wuzijie_1991@163.com>
2016-06-14 15:39:24 +0800

    解决npm start时端口占用的问题

--- scripts/server.js
+++ scripts/server.js
@@ -2,18 +2,17 @@ var koa = require("koa");
 var serve = require("koa-static");
 var path = require("path");
 var Open = require("open");
-
-const httpConfig = {
-  port: 9878,
-  host: "localhost"
-};
+var portfinder = require('portfinder');
 
 let app = koa();
 
 app.use(serve(path.resolve(__dirname, "../demo")));
 app.use(serve(path.resolve(__dirname, "../dist/iife")));
 
-app.listen(httpConfig.port, function() {
-  console.log(`Server is listening at 'http://${httpConfig.host}:${httpConfig.port}/'...`);
-  Open(`http://${httpConfig.host}:${httpConfig.port}/`);
+portfinder.getPort({port: 9878}, function (err, port) {
+  var host = 'localhost';
+  app.listen(port, function() {
+    console.log(`Server is listening at 'http://${host}:${port}/'...`);
+    Open(`http://${host}:${port}/`);
+  });
 });
