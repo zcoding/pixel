42073ebf19a10fc84ec47e6b4d38e270f6df982e
zcoding <wuzijie_1991@163.com>
2016-06-14 16:39:52 +0800

    update

--- dist/cjs/pixel.js
+++ dist/cjs/pixel.js
@@ -29,109 +29,201 @@ babelHelpers.createClass = function () {
 babelHelpers;
 
 /**
- * 求补（异步）
- * r = 255 - r
- * g = 255 - g
- * b = 255 - b
- * a = a
- * @param {Array} data
+ * 求补
+ * r' = 255 - r
+ * g' = 255 - g
+ * b' = 255 - b
+ * a' = a
+ * @param {TypedArray} imageData
  * @return Promise
  */
-function Complement(data) {
+function Complement(imageData) {
+  var N = 100 * 500 * 4;
   return new Promise(function (resolve, reject) {
-    setTimeout(function () {
-      for (var i = 0; i < data.length; i += 4) {
-        data[i] = 255 - data[i];
-        data[i + 1] = 255 - data[i + 1];
-        data[i + 2] = 255 - data[i + 2];
+    // async way
+    function doProcess(pos, len) {
+      for (var i = 0; i < len; i += 4) {
+        imageData[pos + i] = 255 - imageData[pos + i];
+        imageData[pos + i + 1] = 255 - imageData[pos + i + 1];
+        imageData[pos + i + 2] = 255 - imageData[pos + i + 2];
       }
-      resolve(data);
-    }, 0);
+      setTimeout(function () {
+        pos += len;
+        if (pos + N < imageData.length) {
+          doProcess(pos, N);
+        } else if (pos < imageData.length) {
+          doProcess(pos, imageData.length - pos);
+        } else {
+          resolve(imageData);
+        }
+      }, 0);
+    }
+    doProcess(0, N);
+    // sync way
+    // for(let i = 0; i < imageData.length; i += 4) {
+    //   imageData[i] = 255 - imageData[i];
+    //   imageData[i+1] = 255 - imageData[i+1];
+    //   imageData[i+2] = 255 - imageData[i+2];
+    // }
+    // resolve(imageData);
   });
 }
 
 /**
- * 线性运算（异步）
- * r = r * x + y
- * g = g * x + y
- * b = b * x + y
- * a = a
- * @param {Array} data
+ * 线性运算
+ * r' = r * x + y
+ * g' = g * x + y
+ * b' = b * x + y
+ * a' = a
+ * @param {TypedArray} imageData
  * @param {Number} x
  * @param {Number} y
  * @return Promise
  */
-function Linear(data, x, y) {
+function Linear(imageData, x, y) {
+  var N = 100 * 500 * 4;
   return new Promise(function (resolve, reject) {
-    setTimeout(function () {
-      for (var i = 0; i < data.length; i += 4) {
-        data[i] = x * data[i] + y;
-        data[i + 1] = x * data[i + 1] + y;
-        data[i + 2] = x * data[i + 2] + y;
+    // async way
+    function doProcess(pos, len) {
+      for (var i = 0; i < len; i += 4) {
+        imageData[pos + i] = x * imageData[pos + i] + y;
+        imageData[pos + i + 1] = x * imageData[pos + i + 1] + y;
+        imageData[pos + i + 2] = x * imageData[pos + i + 2] + y;
       }
-      resolve(data);
-    }, 0);
+      setTimeout(function () {
+        pos += len;
+        if (pos + N < imageData.length) {
+          doProcess(pos, N);
+        } else if (pos < imageData.length) {
+          doProcess(pos, imageData.length - pos);
+        } else {
+          resolve(imageData);
+        }
+      }, 0);
+    }
+    doProcess(0, N);
+    // sync way
+    // for(let i = 0; i < imageData.length; i += 4) {
+    //   imageData[i] = x * imageData[i] + y;
+    //   imageData[i+1] = x * imageData[i+1] + y;
+    //   imageData[i+2] = x * imageData[i+2] + y;
+    // }
+    // resolve(imageData);
   });
 }
 
 /**
- * 改变透明度（异步）
- * r = r
- * g = g
- * b = b
- * a = opacity * 255
- * @param {Array} data
+ * 改变透明度
+ * r' = r
+ * g' = g
+ * b' = b
+ * a' = a * 255
+ * @param {TypedArray} imageData
  * @param {Number} opacity: 0 ~ 1
  * @return Promise
  */
-function Opacity(data, opacity) {
-  opacity = 225 * opacity;
+function Opacity(imageData, opacity) {
+  var alpha = 225 * opacity;
+  var N = 100 * 500 * 4;
   return new Promise(function (resolve, reject) {
-    setTimeout(function () {
-      for (var i = 0; i < data.length; i += 4) {
-        data[i + 3] = opacity;
+    // async way
+    function doProcess(pos, len) {
+      for (var i = 0; i < len; i += 4) {
+        imageData[pos + i + 3] = alpha;
       }
-      resolve(data);
-    }, 0);
-  });
-}
-
-/**
- * 二值化（异步）
- * r = r < threshold ? 0 : 255
- * g = g < threshold ? 0 : 255
- * b = b < threshold ? 0 : 255
- * a = a
- * @param {Number} threshold
- */
-function Binarization(data, threshold) {
-  return Gray(data).then(function (data) {
-    return new Promise(function (resolve, reject) {
       setTimeout(function () {
-        for (var i = 0; i < data.length; i += 4) {
-          data[i] = data[i + 1] = data[i + 2] = data[i] < threshold ? 0 : 255;
+        pos += len;
+        if (pos + N < imageData.length) {
+          doProcess(pos, N);
+        } else if (pos < imageData.length) {
+          doProcess(pos, imageData.length - pos);
+        } else {
+          resolve(imageData);
         }
-        resolve(data);
       }, 0);
-    });
+    }
+    doProcess(0, N);
+    // sync way
+    // for (let i = 0; i < imageData.length; i += 4) {
+    //   imageData[i+3] = alpha;
+    // }
+    // resolve(imageData);
   });
 }
 
 /**
- * 转换为灰度（异步）
- * r' = g' = b' = 0.299 * r + 0.587 * g + 0.114 * b
+ * 二值化
+ * 先转换为灰度图
+ * c' = c < threshold ? 0 : 255
  * a' = a
+ * @param {TypedArray} imageData
+ * @param {Number} threshold
  * @return Promise
  */
-function Gray(data) {
+function Binarization(imageData, threshold) {
+  var N = 100 * 500 * 4;
   return new Promise(function (resolve, reject) {
-    setTimeout(function () {
-      for (var i = 0; i < data.length; i += 4) {
-        var gray = 0.299 * data[i] + 0.587 * data[i + 1] + 0.114 * data[i + 2];
-        data[i] = data[i + 1] = data[i + 2] = gray;
+    // async way
+    function doProcess(pos, len) {
+      for (var i = 0; i < len; i += 4) {
+        var gray = 0.299 * imageData[pos + i] + 0.587 * imageData[pos + i + 1] + 0.114 * imageData[pos + i + 2];
+        imageData[pos + i] = imageData[pos + i + 1] = imageData[pos + i + 2] = gray < threshold ? 0 : 255;
       }
-      resolve(data);
-    }, 0);
+      setTimeout(function () {
+        pos += len;
+        if (pos + N < imageData.length) {
+          doProcess(pos, N);
+        } else if (pos < imageData.length) {
+          doProcess(pos, imageData.length - pos);
+        } else {
+          resolve(imageData);
+        }
+      }, 0);
+    }
+    doProcess(0, N);
+    // sync way
+    // for(let i = 0; i < imageData.length; i += 4) {
+    //   var gray = 0.299 * imageData[i] + 0.587 * imageData[i+1] + 0.114 * imageData[i+2];
+    //   imageData[i] = imageData[i+1] = imageData[i+2] = gray < threshold ? 0 : 255;
+    // }
+    // resolve(imageData);
+  });
+}
+
+/**
+ * 转换为灰度
+ * r' = g' = b' = 0.299 * r + 0.587 * g + 0.114 * b
+ * a' = a
+ * @param {TypedArray} imageData
+ * @return Promise
+ */
+function Gray(imageData) {
+  var N = 100 * 500 * 4;
+  return new Promise(function (resolve, reject) {
+    // async way
+    function doProcess(pos, len) {
+      for (var i = 0; i < len; i += 4) {
+        var gray = 0.299 * imageData[pos + i] + 0.587 * imageData[pos + i + 1] + 0.114 * imageData[pos + i + 2];
+        imageData[pos + i] = imageData[pos + i + 1] = imageData[pos + i + 2] = gray;
+      }
+      setTimeout(function () {
+        pos += len;
+        if (pos + N < imageData.length) {
+          doProcess(pos, N);
+        } else if (pos < imageData.length) {
+          doProcess(pos, imageData.length - pos);
+        } else {
+          resolve(imageData);
+        }
+      }, 0);
+    }
+    doProcess(0, N);
+    // sync way
+    // for(let i = 0; i < imageData.length; i += 4) {
+    //   var gray = 0.299 * imageData[i] + 0.587 * imageData[i+1] + 0.114 * imageData[i+2];
+    //   imageData[i] = imageData[i+1] = imageData[i+2] = gray;
+    // }
+    // resolve(imageData);
   });
 }
 
--- dist/iife/pixel.js
+++ dist/iife/pixel.js
@@ -192,7 +192,7 @@ var Pixel = (function () {
   }
 
   /**
-   * 转换为灰度（异步）
+   * 转换为灰度
    * r' = g' = b' = 0.299 * r + 0.587 * g + 0.114 * b
    * a' = a
    * @param {TypedArray} imageData
